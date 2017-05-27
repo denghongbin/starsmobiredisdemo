746f840fe8529dfd114138b7028700d09adf2768
denghongbin <hongbin.deng@happylifeplat.com>
2017-05-27 14:36:14 +0800

    redis
    redis demo

---
+++ src/main/java/com/starsmobi/StarsmobiredisdemoApplication.java
@@ -0,0 +1,12 @@
+package com.starsmobi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarsmobiredisdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarsmobiredisdemoApplication.class, args);
+	}
+}
